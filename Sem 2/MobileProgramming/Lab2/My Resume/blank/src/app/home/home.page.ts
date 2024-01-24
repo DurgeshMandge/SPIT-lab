53ee69cbea02c8e35eea3bc045f857fc2bbd5a88
Durgesh Mandge <durgeshdmd4602@gmail.com>
2024-01-24 13:12:02 +0530

    links achievement qualification

--- Sem 2/MobileProgramming/Lab2/My Resume/blank/src/app/home/home.page.ts
+++ Sem 2/MobileProgramming/Lab2/My Resume/blank/src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonAvatar,IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent ,IonCheckbox} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonAvatar,IonCard,
+  IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent ,IonCheckbox,IonGrid,IonRow,IonCol} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonAvatar, IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent,IonCheckbox],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonAvatar, 
+    IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent,IonCheckbox,IonGrid,IonRow,IonCol],
 })
 export class HomePage {
   constructor() {}
