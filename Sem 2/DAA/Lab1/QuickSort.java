1b9e0fedc550b5b739a49956cdb3a3416cf92c87
Durgesh Mandge <durgeshdmd4602@gmail.com>
2024-01-30 17:12:24 +0530

    lab2daa

--- Sem 2/DAA/Lab1/QuickSort.java
+++ Sem 2/DAA/Lab1/QuickSort.java
@@ -1,12 +1,43 @@
 package Lab1;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
-    // r + (r+1)*i 
-    // 32 + 33*i
-    // = [ 64 96 128 160 192 224 256 288 320 352 ]
-    public static void main(String[] args) {
-        int[] arr = {5, 4, 2, 7, 43, 34, 11, 98};
-        
-    }
+	public static void main(String[] args){
+		int[] arr = { 128, 192, 64, 288, 352, 160, 96, 256, 320, 224 };
+		int N = arr.length;
+
+		quickSort(arr, 0, N - 1);
+        System.out.println(Arrays.toString(arr));
+	}
+
+	static void swap(int[] arr, int i, int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	static int partition(int[] arr, int low, int high){
+		int pivot = arr[high];
+		int i = (low - 1);
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i + 1);
+	}
+
+	static void quickSort(int[] arr, int low, int high){
+		if (low < high) {
+			int pi = partition(arr, low, high);
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+	}
+    
 }
+
